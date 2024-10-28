f985639a23d94dded2e1599c3080179d21754cba
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-28 16:11:22 +0530

    React-Converts-Klinik-Template

--- clinik-template/src/App.js
+++ clinik-template/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Test from './Folder/10-Test/Test';
+import Footer from './Folder/11-Footer/Footer';
+import Navbar from './Folder/3-Nav/Navbar';
+import Header from './Folder/4-Header/Header';
+import About from './Folder/5-About/About';
+import Service from './Folder/6-Service/Service';
+import Future from './Folder/7-Future/Future';
+import Team from './Folder/8-Team/Team';
+import Appointment from './Folder/9-Appointment/Appointment';
+import Topsection from './Folder/2-TOP/Topsection';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topsection/>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Service/>
+      <Future/>
+      <Team/>
+      <Appointment/>
+      <About/>
+      <Test/>
+      <Footer/>
+     
+
+    
     </div>
   );
 }
