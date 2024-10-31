f5bb5d9ed657c67f786faa842c35b8d1bbf2564e
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-31 22:11:40 +0530

    React-Converts-Klinik-template

--- clinik-template/src/App.js
+++ clinik-template/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Test from './Folder/10-Test/Test';
 import Footer from './Folder/11-Footer/Footer';
 import Navbar from './Folder/3-Nav/Navbar';
 import Header from './Folder/4-Header/Header';
@@ -9,6 +8,7 @@ import Future from './Folder/7-Future/Future';
 import Team from './Folder/8-Team/Team';
 import Appointment from './Folder/9-Appointment/Appointment';
 import Topsection from './Folder/2-TOP/Topsection';
+import Testsection from './Folder/10-Test/Testsection';
 
 function App() {
   return (
@@ -22,11 +22,8 @@ function App() {
       <Team/>
       <Appointment/>
       <About/>
-      <Test/>
+      <Testsection/>
       <Footer/>
-     
-
-    
     </div>
   );
 }
